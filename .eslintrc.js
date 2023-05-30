36720d675ed9e679e62b5b6e02e6b55e589b2347
Alysson Oliveira Santos <alyssonn.bra@live.com>
2023-05-30 10:58:40 -0300

    Structuring project

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: 'airbnb-base',
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-console': 'off',
+    'class-methods-use-this': 'off',
+    'linebreak-style': 'off',
+    'import/no-extraneous-dependencies': 'off',
+    'no-unused-vars': 'off',
+  },
+};
