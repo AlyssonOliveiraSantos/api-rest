d547d2bd603b78865f015f4fcb4100b5246a259b
Alysson Oliveira Santos <alyssonn.bra@live.com>
2023-06-12 16:49:28 -0300

    Handling uploads

--- src/config/multer.js
+++ src/config/multer.js
@@ -4,13 +4,20 @@ import { extname, resolve } from 'path';
 const aleatorio = () => Math.floor(Math.random() * 10000 + 10000);
 
 export default {
-  Storage: multer.diskStorage({
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg') {
+      return cb(new multer.MulterError('Arquivo precisa ser png ou jpg'));
+    }
+
+    return cb(null, true);
+  },
+
+  storage: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, resolve(__dirname, '..', '..', 'uploads'));
     },
     filename: (req, file, cb) => {
-      cb(null, `${Date.now()}_${aleatorio}${extname(file.originalname)}`);
+      cb(null, `${Date.now()}_${aleatorio()}${extname(file.originalname)}`);
     },
-
   }),
 };
--- src/controllers/PhotoController.js
+++ src/controllers/PhotoController.js
@@ -1,6 +1,17 @@
+import multer from 'multer';
+import multerConfig from '../config/multer';
+const upload = multer(multerConfig).single('foto');
+
 class PhotoController {
   async store(req, res) {
-    res.json(req.file);
+    return upload(req, res, (err) => {
+      if (err) {
+        res.status(401).json({
+          errors: [err],
+        });
+        res.json(req.file);
+      }
+    });
   }
 }
 
